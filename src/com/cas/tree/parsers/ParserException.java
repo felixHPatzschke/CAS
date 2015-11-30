cb0d5fc9b40863a099f661cc9722857c72018b25
felixHPatzschke <felix.patzschke@gmail.com>
2015-11-30 22:53:59 +0100

    Introduced debug messages in ParserException

--- src/com/cas/tree/parsers/ParserException.java
+++ src/com/cas/tree/parsers/ParserException.java
@@ -4,4 +4,13 @@ package com.cas.tree.parsers;
  * Created by user on 29/11/15.
  */
 public class ParserException extends Exception {
+
+    public ParserException(){
+        super();
+    }
+
+    public ParserException(String msg){
+        super(msg);
+    }
+
 }

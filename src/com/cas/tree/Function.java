d28a666b4116fc27b504efcf08d646696dc32aae
felixHPatzschke <felix.patzschke@gmail.com>
2015-12-01 16:39:02 +0100

    Introduced Function class for user-defined functions, maybe default functions as well location of class is very likely to change

---
+++ src/com/cas/tree/Function.java
@@ -0,0 +1,28 @@
+package com.cas.tree;
+
+import com.cas.namespace.Identifier;
+import com.cas.namespace.LocalNamespace;
+import com.cas.tree.parsers.NotCompletelyParsedException;
+import java.util.ArrayList;
+
+/**
+ * Created by felix on 01.12.2015.
+ */
+public class Function implements Term {
+
+    LocalNamespace namespace;
+    Identifier name;
+    ArrayList<Term> args;
+
+
+    @Override
+    public String getStringRepresentation() {
+        return name.toString();
+    }
+
+    @Override
+    public boolean isNumber() throws NotCompletelyParsedException {
+        return false;
+    }
+
+}

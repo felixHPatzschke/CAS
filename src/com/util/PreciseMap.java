09d151cd5a3c151ee8b1dd4c9ed84e63d8635ae6
felixHPatzschke <felix.patzschke@gmail.com>
2015-12-03 16:51:53 +0100

    Added JavaDoc to PreciseMap

--- src/com/util/PreciseMap.java
+++ src/com/util/PreciseMap.java
@@ -19,26 +19,51 @@ public class PreciseMap<KeyType, ValueType> {
     }
 
 
+    /**
+     * @return the number of entries currently listed in the Map
+     */
     public int size() {
         return highestIndex+1;
     }
 
+    /**
+     * @return true, if there are no entries listed in the Map
+     */
     public boolean isEmpty() {
         return (highestIndex==0);
     }
 
+    /**
+     * @param key The key to be searched for
+     * @return true, if the given key is listed in the Map
+     */
     public boolean containsKey(KeyType key) {
         return keys.contains(key);
     }
 
+    /**
+     * @param value the value to be searched for
+     * @return true, if the given Value is listed in the Map
+     */
     public boolean containsValue(ValueType value) {
         return values.contains(value);
     }
 
+    /**
+     * Fetches a value from the Map
+     * @param key the identifying key
+     * @return the value, identified by the key
+     */
     public ValueType get(KeyType key) {
         return values.get(keys.indexOf(key));
     }
 
+    /**
+     * Adds an entry to the Map
+     * @param key the identifier
+     * @param value the value
+     * @return the value, which was formerly associated with the given key, null if it was not defined
+     */
     public ValueType put(KeyType key, ValueType value) {
         if(keys.contains(key)){
             int i = keys.indexOf(key);
@@ -53,6 +78,11 @@ public class PreciseMap<KeyType, ValueType> {
         }
     }
 
+    /**
+     * Removes an entry from the Map
+     * @param key the identifier of the entry to be deleted
+     * @return the value which was associated with the given key
+     */
     public ValueType remove(KeyType key) {
         int i = keys.indexOf(key);
         ValueType res = values.get(i);
@@ -69,20 +99,32 @@ public class PreciseMap<KeyType, ValueType> {
         return res;
     }
 
+    /**
+     * Deletes all entries
+     */
     public void clear() {
         keys.clear();
         values.clear();
         highestIndex = 0;
     }
 
+    /**
+     * @return A List of all keys listed in the Map
+     */
     public List<KeyType> keys() {
         return keys;
     }
 
+    /**
+     * @return a List of all values listed in the Map
+     */
     public List<ValueType> values() {
         return values;
     }
 
+    /**
+     * @return a List of all key-value-pairs listed in the Map
+     */
     public List<Map.Entry<KeyType, ValueType>> entries(){
         ArrayList<Map.Entry<KeyType, ValueType>> res = new ArrayList<Map.Entry<KeyType, ValueType>>();
         for(int c=0; c<=highestIndex; c++){

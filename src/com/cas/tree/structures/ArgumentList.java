47d2cdc643aa69e29599035a747bdb25fcba3b5a
felixHPatzschke <felix.patzschke@gmail.com>
2015-12-09 18:42:41 +0100

    Introduced Structure Interface and ArgumentList

---
+++ src/com/cas/tree/structures/ArgumentList.java
@@ -0,0 +1,8 @@
+package com.cas.tree.structures;
+
+/**
+ * Created by Felix Patzschke on 09.12.2015.
+ */
+public class ArgumentList implements Structure {
+
+}
---
+++ src/com/cas/tree/structures/Structure.java
@@ -0,0 +1,10 @@
+package com.cas.tree.structures;
+
+import java.util.HashMap;
+
+/**
+ * Created by Felix Patzschke on 09.12.2015.
+ */
+public interface Structure {
+
+}
